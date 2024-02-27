ddd8d325c4294a5343aa716798bbe05239f4da8f
annarusak <alitvinetz@gmail.com>
2024-02-27 16:14:28 +0400

    Add documentations

--- WeatherApp/controller/ViewController.swift
+++ WeatherApp/controller/ViewController.swift
@@ -3,11 +3,15 @@ import Foundation
 
 class ViewController: UIViewController, UIScrollViewDelegate {
     
+    // MARK: - Properties
+    
+    /// Enum for font names
     enum FontName: String {
         case copperplate = "Copperplate"
         case helveticaNeue = "Helvetica Neue"
     }
     
+    /// Image view for displaying a circle image
     private let circleImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "circle")
@@ -15,6 +19,7 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         return imageView
     }()
     
+    // Labels for weather information
     private lazy var cityNameLabel = createLabel(labelText: "-", labelColor: .black, fontName: FontName.copperplate.rawValue, sizeOfFont: 20)
     private lazy var temperatureMainLabel = createLabel(labelText: "", labelColor: .black, fontName: FontName.helveticaNeue.rawValue, sizeOfFont: 43)
     private lazy var dayOfWeekLabel = createLabel(labelText: "-", labelColor: .black, fontName: FontName.copperplate.rawValue, sizeOfFont: 30)
@@ -25,15 +30,20 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     private lazy var humidityLabel = createLabel(imageName: "humidity", imageColor: .humidityIconColor, labelText: "-", fontName: FontName.helveticaNeue.rawValue, sizeOfFont: 17)
     private lazy var weatherForecastLabel = createLabel(labelText: "10-DAY WEATHER FORECAST", labelColor: .lightGray, fontName: FontName.copperplate.rawValue, sizeOfFont: 14)
     
-    
+    /// ScrollView for displaying weather forecast
     private var scrollView = UIScrollView()
     private var weatherForecastSubviews: [ScrollViewElement] = []
     
+    /// Location manager instance for managing device's location
     private let locationManager = LocationManager()
+    
+    /// Weather provider instance for fetching weather data
     private let weatherProvider = WeatherProvider()
     
+    /// Current date
     private let date = Date()
     
+    // MARK: - View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,7 +61,15 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         circleRotation()
     }
     
+    // MARK: - Helper Methods
     
+    /// Create a label with given parameters.
+    /// - Parameters:
+    ///   - labelText: The text to be displayed on the label.
+    ///   - labelColor: The color of the label's text.
+    ///   - fontName: The name of the font to be used.
+    ///   - sizeOfFont: The size of the font.
+    /// - Returns: UILabel instance with specified parameters.
     private func createLabel(labelText: String, labelColor: UIColor, fontName: String, sizeOfFont: CGFloat) -> UILabel {
         let label = UILabel()
         label.text = labelText
@@ -61,14 +79,22 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         return label
     }
     
+    /// Create a label with an icon and given parameters.
+    /// - Parameters:
+    ///   - imageName: The name of the image icon.
+    ///   - imageColor: The color of the image icon.
+    ///   - labelText: The text to be displayed on the label.
+    ///   - fontName: The name of the font to be used.
+    ///   - sizeOfFont: The size of the font.
+    /// - Returns: UILabel instance with specified parameters.
     private func createLabel(imageName: String, imageColor: UIColor,
                              labelText: String, fontName: String,
                              sizeOfFont: CGFloat) -> UILabel {
         let label = UILabel()
         let imageAttachment = NSTextAttachment()
         imageAttachment.image = UIImage.init(systemName: imageName)?
-                                                .withRenderingMode(.alwaysTemplate)
-                                                .withTintColor(imageColor)
+            .withRenderingMode(.alwaysTemplate)
+            .withTintColor(imageColor)
         let attributedString = NSMutableAttributedString(string: " \(labelText)")
         attributedString.insert(NSAttributedString(attachment: imageAttachment), at: 0)
         label.attributedText = attributedString
@@ -78,6 +104,7 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         return label
     }
     
+    /// Setup the scroll view to display weather forecast.
     private func setupScrollView() {
         let screenWidth = UIScreen.main.bounds.width
         let screenHeight = UIScreen.main.bounds.height
@@ -111,6 +138,7 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         view.addSubview(scrollView)
     }
     
+    /// Set up the user interface elements.
     private func setupViews() {
         view.backgroundColor = .white
         view.addSubview(circleImageView)
@@ -133,24 +161,24 @@ class ViewController: UIViewController, UIScrollViewDelegate {
             
             dayOfWeekLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             dayOfWeekLabel.bottomAnchor.constraint(equalTo: dayOfMonthLabel.topAnchor, constant: -5),
-
+            
             circleImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             circleImageView.bottomAnchor.constraint(equalTo: dayOfWeekLabel.topAnchor, constant: -50),
             circleImageView.widthAnchor.constraint(equalToConstant: 160),
             circleImageView.heightAnchor.constraint(equalToConstant: 160),
-
+            
             temperatureMainLabel.centerXAnchor.constraint(equalTo: circleImageView.centerXAnchor, constant: 5),
             temperatureMainLabel.centerYAnchor.constraint(equalTo: circleImageView.centerYAnchor, constant: -3),
-
+            
             cityNameLabel.centerXAnchor.constraint(equalTo: circleImageView.centerXAnchor),
             cityNameLabel.bottomAnchor.constraint(equalTo: circleImageView.topAnchor, constant: -5),
-
+            
             windSpeedLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             windSpeedLabel.centerYAnchor.constraint(equalTo: weatherConditionsLabel.centerYAnchor, constant: 100),
-
+            
             temperatureLabel.centerYAnchor.constraint(equalTo: windSpeedLabel.centerYAnchor),
             temperatureLabel.rightAnchor.constraint(equalTo: windSpeedLabel.leftAnchor, constant: -60),
-
+            
             humidityLabel.centerYAnchor.constraint(equalTo: windSpeedLabel.centerYAnchor),
             humidityLabel.leftAnchor.constraint(equalTo: windSpeedLabel.rightAnchor, constant: 60),
             
@@ -159,38 +187,52 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         ])
     }
     
+    /// Rotate the circle image view.
     private func circleRotation() {
-        // Create a CABasicAnimation for rotation
+        /// Create a CABasicAnimation for rotation
         let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
         rotationAnimation.toValue = NSNumber(value: Double.pi * 2.0)
         rotationAnimation.duration = 1.0
         circleImageView.layer.add(rotationAnimation, forKey: "spinAnimation")
     }
     
+    /// Update the weather label with either an icon or plain text.
+    /// - Parameters:
+    ///   - label: The label to be updated.
+    ///   - value: The value to be displayed.
+    ///   - postfix: The postfix to be added after the value.
+    ///   - withIcon: A  boolean value indicating whether to use an icon.
     private func updateWeatherLabel(label: UILabel, value: Float, postfix: String, withIcon: Bool) {
         let intValue = Int(value.rounded())
-
+        
         if (withIcon) {
             guard let existingAttributedString = label.attributedText?.mutableCopy() as? NSMutableAttributedString else {
                 return
             }
-
-            // Find and modify the text part
+            
+            /// Find and modify the text part
             let iconLength = 1
             let range = NSRange(location: iconLength, length: existingAttributedString.length - iconLength)
             existingAttributedString.replaceCharacters(in: range, with: " \(intValue)" + postfix)
-
-            // Set the updated attributed text back to the label
+            
+            /// Set the updated attributed text back to the label
             label.attributedText = existingAttributedString
         } else {
             label.text = String(intValue) + postfix
         }
     }
     
+    /// Update the weather label with text.
+    /// - Parameters:
+    ///   - label: The label to be updated.
+    ///   - value: The text value to be displayed.
     private func updateWeatherLabel(label: UILabel, value: String) {
         label.text = value
     }
-
+    
+    /// Extracts city name from timezone.
+    /// - Parameter timezone: The timezone string.
+    /// - Returns: The extracted city name.
     private func cityFromTimezone(timezone: String) -> String {
         var afterSlash = ""
         if let slash = timezone.range(of: "/")?.upperBound {
@@ -202,6 +244,8 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         return afterSlash
     }
     
+    /// Handles current weather data delegate method.
+    /// - Parameter weather: The current weather data.
     private func currentWeatherDelegate(weather: WeatherProvider.WeatherCurrent) {
         updateWeatherLabel(label: temperatureMainLabel, value: weather.temperature, postfix: "°", withIcon: false)
         updateWeatherLabel(label: temperatureLabel, value: weather.temperature, postfix: "°", withIcon: true)
@@ -213,23 +257,28 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         updateWeatherLabel(label: dayOfWeekLabel, value: date.dayOfWeek() ?? "")
     }
     
+    /// Handles weather forecast data delegate method.
+    /// - Parameter weatherForecast: The weather forecast data.
     private func weatherForecastDelegate(weatherForecast: [WeatherProvider.WeatherForecast]) {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
         var dayOfWeek = ""
         
         for (index, day) in weatherForecast.enumerated() {
-                if let date = dateFormatter.date(from: day.datetime) {
+            if let date = dateFormatter.date(from: day.datetime) {
                 dayOfWeek = date.dayOfWeek() ?? day.datetime
             }
             if index <= weatherForecastSubviews.count {
-            weatherForecastSubviews[index].updateView(dayOfWeek: dayOfWeek,
-                                                      temperature: Int((day.temperature).rounded()),
-                                                      icon: forecastIcon(iconName: day.icon))
+                weatherForecastSubviews[index].updateView(dayOfWeek: dayOfWeek,
+                                                          temperature: Int((day.temperature).rounded()),
+                                                          icon: forecastIcon(iconName: day.icon))
             }
         }
     }
     
+    /// Returns the appropriate weather icon image based on the given icon name.
+    /// - Parameter iconName: The name of the weather icon.
+    /// - Returns: The UIImage representing the weather icon.
     private func forecastIcon(iconName: String) -> UIImage {
         switch iconName {
         case IconName.clearDay.rawValue:
@@ -254,10 +303,12 @@ class ViewController: UIViewController, UIScrollViewDelegate {
             return UIImage(named: iconName)!
         }
     }
-
+    
+    /// Fetches weather data for a given location.
+    /// - Parameter location: The latitude and longitude of the location.
     private func requestWeatherForLocation(location : (latitude: Double, longitude: Double)) {
         print("\(location.latitude) | \(location.longitude)")
         weatherProvider.request(location: location)
     }
-
+    
 }
--- WeatherApp/model/WeatherProvider.swift
+++ WeatherApp/model/WeatherProvider.swift
@@ -1,8 +1,9 @@
 import Foundation
 
-
+// A class responsible for providing weather information.
 class WeatherProvider {
     
+    // Structure representing current weather conditions.
     struct WeatherCurrent {
         var temperature: Float = 0
         var windSpeed: Float = 0
@@ -11,25 +12,34 @@ class WeatherProvider {
         var timezone: String = ""
     }
     
+    // Structure representing weather forecast for a day.
     struct WeatherForecast {
         var datetime: String = ""
         var temperature: Float = 0
         var icon: String = ""
     }
     
+    /// The current weather conditions.
     private var weatherCurrent = WeatherCurrent()
+    
+    /// The weather forecast for the next 10 days.
     private var weatherForecast10Days: [WeatherForecast] = []
     
+    /// The API key for accessing weather data.
     private let keyURLString = "SEGQ9YES8T55WUMCE8CNRVCJR"
     
+    /// A closure to handle current weather data updates.
     private var delegateWeatherCurrent: ((WeatherCurrent) -> Void)?
+    
+    /// A closure to handle weather forecast data updates.
     private var delegateWeatherForecast: (([WeatherForecast]) -> Void)?
     
+    /// Requests weather information for a given location.
+    /// - Parameter location: A tuple containing latitude and longitude.
     func request(location : (latitude: Double, longitude: Double)) {
         
         let urlString = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + String(location.latitude) + "%2C" + String(location.longitude) + "?unitGroup=metric&key=" + keyURLString + "&contentType=json"
         
-        // Doing URL request, check and parse our json, assign weather values we need to variables we made at the beginning
         guard let url = URL(string: urlString) else { return }
         
         URLSession.shared.dataTask(with: url) { [self] data, response, error in
@@ -41,16 +51,19 @@ class WeatherProvider {
             guard let data = data else { return }
             let jsonString = String(data: data, encoding: .utf8)
             print(jsonString!)
-
+            
             do {
+                /// Decode the JSON response into WeatherDetails structure
                 let decoder = JSONDecoder()
                 let details = try decoder.decode(WeatherDetails.self, from: data)
                 
+                /// Update the weatherCurrent structure with current weather details
                 weatherCurrent = WeatherCurrent(temperature: details.currentConditions.temp,
-                                      windSpeed: details.currentConditions.windspeed,
-                                      humidity: details.currentConditions.humidity,
-                                      conditions: details.currentConditions.conditions,
-                                      timezone: details.timezone)
+                                                windSpeed: details.currentConditions.windspeed,
+                                                humidity: details.currentConditions.humidity,
+                                                conditions: details.currentConditions.conditions,
+                                                timezone: details.timezone)
+                /// Parse the weather forecast for the next 10 days
                 for i in 1...10 {
                     weatherForecast10Days.append(WeatherForecast(datetime: details.days[i].datetime,
                                                                  temperature: details.days[i].temp,
@@ -60,16 +73,21 @@ class WeatherProvider {
                 print(error)
             }
             DispatchQueue.main.async {
+                /// Call the delegate closure to pass the current weather data and weather forecast data
                 self.delegateWeatherCurrent?(weatherCurrent)
                 self.delegateWeatherForecast?(weatherForecast10Days)
             }
         }.resume()
     }
     
+    /// Adds a delegate closure to handle current weather updates.
+    /// - Parameter delegate: A closure taking a `WeatherCurrent` object as a parameter.
     func addDelegateWeatherCurrent(delegate: @escaping (WeatherCurrent) -> Void) {
         self.delegateWeatherCurrent = delegate
     }
     
+    /// Adds a delegate closure to handle weather forecast updates.
+    /// - Parameter delegate: A closure taking an array of `WeatherForecast` objects as a parameter.
     func addDelegateWeatherForecast(delegate: @escaping ([WeatherForecast]) -> Void) {
         self.delegateWeatherForecast = delegate
     }
